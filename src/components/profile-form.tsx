519fe7d2bd421bd6df2c32d99b85b502b14f49af
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 17:31:14 -0300

    manages form state with formik

--- src/components/profile-form.tsx
+++ src/components/profile-form.tsx
@@ -1,35 +1,69 @@
-import React from 'react';
-import { Container, Header, Content, Form, Item, Input, Button, Text } from 'native-base';
+import React, { ReactChild } from 'react';
+import { useFormik } from 'formik';
+import { styles } from '../assets/style';
+import { View } from 'react-native';
+import { Button, Input } from 'react-native-elements';
 
 type ProfileFormProps = {
-  action: string
+  action: string,
+  firstName?: string,
+  lastName?: string,
+  email?: string,
+  password?: string,
+  children?: ReactChild
 }
 
-export class ProfileForm extends React.Component<ProfileFormProps> {
-  render() {
-    return (
-      <Container>
-        <Form>
-          <Item>
-            <Input placeholder="Nome" />
-          </Item>
-          <Item>
-            <Input placeholder="Sobrenome" />
-          </Item>
-          <Item>
-            <Input placeholder="Email" />
-          </Item>
-          <Item>
-            <Input placeholder="Senha" />
-          </Item>
-        </Form>
+export const ProfileForm = (props: ProfileFormProps) => {
+
+  const formik = useFormik({
+   initialValues: {
+     firstName: props.firstName ? props.firstName : '',
+     lastName: props.lastName ? props.lastName : '',
+     email: props.email ? props.email : '',
+     password: props.password ? props.firstName : ''
+   },
+   onSubmit: values => {
+     console.log(values)
+   }
+  })
+
+  return (
+    <View
+      style={{ ...styles.stretchContainer, ...styles.justifyBetween}}
+    >
+      <View>
+        <Input
+          placeholder="Nome"
+          value={formik.values.firstName}
+          onChangeText={text => formik.setFieldValue('firstName', text)}
+        />
+        <Input
+          placeholder="Sobrenome"
+          value={formik.values.lastName}
+          onChangeText={text => formik.setFieldValue('lastName', text)}
+        />
+        <Input
+          placeholder="Email"
+          value={formik.values.email}
+          onChangeText={text => formik.setFieldValue('email', text)}
+        />
+        <Input
+          placeholder="Senha"
+          value={formik.values.password}
+          onChangeText={text => formik.setFieldValue('password', text)}
+          secureTextEntry
+        />
+      </View>
+      <View>
         <Button
-          rounded
-          block
-        >
-          <Text>{this.props.action}</Text>
-        </Button>
-      </Container>
-    )
-  }
+          buttonStyle={styles.roundButton}
+          containerStyle={{...styles.marginHorizontalMd, ...styles.marginVerticalMd}}
+          title={props.action}
+          raised
+        />
+        { props.children && props.children }
+      </View>
+    </View>
+  )
+
 }
