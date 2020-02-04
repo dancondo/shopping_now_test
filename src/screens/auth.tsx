b056ee4eef9aa280a2e87e088ae388dbbf3127f9
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 12:10:23 -0300

    Creates two basic screens auth and home

---
+++ src/screens/auth.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from "react-native";
+
+export class AuthScreen extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <SafeAreaView>
+          <Text>HOME SCREEN</Text>
+        </SafeAreaView>
+      </React.Fragment>
+    )
+  }
+}
---
+++ src/screens/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from "react-native";
+
+export class HomeScreen extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <SafeAreaView>
+          <Text>HOME SCREEN</Text>
+        </SafeAreaView>
+      </React.Fragment>
+    )
+  }
+}
