b08ca16c0e309db1725fc4ece9aee2252b593265
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-05 16:23:13 -0300

    creates simple navigation to profile page

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { SafeAreaView, Text } from "react-native";
 import { useSelector } from 'react-redux';
+import { ThemeProvider, Button } from 'react-native-elements';
+import { useNavigation } from '../hooks/use-navigation';
 
 export const HomeScreen = () => {
   const routeName = 'Home'
+  const navigation = useNavigation();
   const user = useSelector(state => state.auth.user);
 
   return (
-    <React.Fragment>
+    <ThemeProvider>
       <SafeAreaView>
-        <Text>{user.firstName}</Text>
+        <Button
+          title={user.firstName}
+          onPress={() => navigation.navigate('Profile')}
+        />
       </SafeAreaView>
-    </React.Fragment>
+    </ThemeProvider>
   )
 }
