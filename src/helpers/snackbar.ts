19c28cf903a2c2708598e79639071a295ad05342
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-06 18:26:25 -0300

    add success message option to snackbar

--- src/helpers/snackbar.ts
+++ src/helpers/snackbar.ts
@@ -1,8 +1,13 @@
 import Snackbar from 'react-native-snackbar';
 
-export const snackbarAsyncWrapper = async (fn: Function) => {
+export const snackbarAsyncWrapper = async (fn: Function, successMessage?: string) => {
   try {
     await fn();
+    if(successMessage) {
+      Snackbar.show({
+        text: successMessage
+      })
+    }
   } catch (err) {
     Snackbar.show({
       text: err.message
