eeef5b18479fa1544c4d7516264986710395dd3f
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-06 13:09:56 -0300

    adds toggle loading

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -2,22 +2,29 @@ import { AuthActions } from "../actions/auth";
 
 const initialState = {
   token: null,
-  user: null
+  user: null,
+  loading: false
 };
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case AuthActions.setAuthData:
       return {
+        ...state,
         token: action.payload.token,
         user: action.payload.user
       }
     case AuthActions.update:
-        return {
-          user: action.payload,
-          token: state.token
-        }
-      default:
-        return state;
+      return {
+        ...state,
+        user: action.payload
+      }
+    case AuthActions.toggleLoading:
+      return {
+        ...state,
+        loading: !state.loading
+      }
+    default:
+      return state;
   }
 }
